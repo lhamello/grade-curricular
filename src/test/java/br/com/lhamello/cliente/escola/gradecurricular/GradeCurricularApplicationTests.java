2fd551b3a5b408747d080d908c6a8b5b695c9a22
Luiz Henrique A. Mello <lhamello@gmail.com>
2021-01-30 01:53:50 -0300

    Projeto criado

---
+++ src/test/java/br/com/lhamello/cliente/escola/gradecurricular/GradeCurricularApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lhamello.cliente.escola.gradecurricular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeCurricularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
